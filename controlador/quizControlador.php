1582c096db62be3cc4c2de92d1ec59fce441c06c
rafaeldamiam <rafael.damiam23@gmail.com>
2021-01-20 13:00:07 -0300

    Update quizControlador.php
    correções

--- controlador/quizControlador.php
+++ controlador/quizControlador.php
@@ -5,19 +5,25 @@ require_once "modelo/quizModelo.php";
 /** anon */
 function adicionar() {
     if (ehPost()) {
-        $quiz["quest"] = $_POST["quest"];
-        $quiz["r"] = $_POST["r"];
-        $quiz["rcerta"] = $_POST["rcerta"];
-        $quiz["id"] = $_POST["id"];
-        /* dd($quiz); */
-        if($quiz["r"] == $quiz["rcerta"]){
-            echo("acertou");
-            /* alert(acertou()); */
-            $dados["quiz"] = sortearPegarQuestao();
-            exibir("quiz/formulario", $dados);
+        if(!empty($_POST["r"])){
+            $quiz["quest"] = $_POST["quest"];
+            $quiz["r"] = $_POST["r"];
+            $quiz["rcerta"] = $_POST["rcerta"];
+            $quiz["id"] = $_POST["id"];
+            /* dd($quiz); */
+            if($quiz["r"] == $quiz["rcerta"]){
+                echo("<script>alert('Correto');</script>");
+                /* alert(acertou()); */
+                $dados["quiz"] = sortearPegarQuestao();
+                exibir("quiz/formulario", $dados);
+            }else{
+                echo("<script>alert('Errado');</script>");
+                /* alert(errou()); */
+                $dados["quiz"] = sortearPegarQuestao();
+                exibir("quiz/formulario", $dados);
+            }
         }else{
-            echo("errou");
-            /* alert(errou()); */
+            echo("<script>alert('Escolha alguma alternativa');</script>");
             $dados["quiz"] = sortearPegarQuestao();
             exibir("quiz/formulario", $dados);
         }
