146841292178aaf059b2ec6e1af779a963dde122
LucasCavalherie <lucascavalherie01@gmail.com>
2021-04-04 23:10:21 -0300

    Att finais

--- visao/paginas/inicial.visao.php
+++ visao/paginas/inicial.visao.php
@@ -3,36 +3,81 @@
             <div class="text-center mb-5 col-12">
                 <h1 class="font-weight-bold text-white">Parabéns Camileeeeeeeee!!!</h1>
                 <h3 class="font-weight-bold text-white">Hoje é o seu diaaa!!! </h3>
+                <br>
+                <h4 class="font-weight text-white">Como já dizia a filosofa contemporanea <b>Xuxa</b> : 
+                Hoje vai ser uma festa, 
+                bolo e guaraná,
+                muito doce pra você,
+                é o seu aniversário,
+                vamos festejar
+                e os amigos receber <br> (Online né)</h4>
             </div>
         </div>
     </div>
 
     <div id="carouselExampleIndicators" class="carousel slide mb-5" data-ride="carousel">
-        <ol class="carousel-indicators">
+        <!-- <ol class="carousel-indicators">
             <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
             <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
             <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
+        </ol> -->
         <div class="carousel-inner">
             <div class="carousel-item active">
                 <img class="d-block w-100" src="./publico/imgs/img1.jpeg" alt="First slide">
                 <div class="carousel-caption d-none d-md-block m-auto col-4 rounded bg-caption">
-                    <h5>Rolezão</h5>
-                    <p>Nesse dia nós comemos coxinhão</p>
+                    <h5>Bffs</h5>
                 </div>
             </div>
             <div class="carousel-item">
                 <img class="d-block w-100" src="./publico/imgs/img2.jpeg" alt="Second slide">
                 <div class="carousel-caption d-none d-md-block m-auto col-4 rounded bg-caption">
-                    <h5>Rolezin</h5>
-                    <p>Nesse dia nós comemos coxinha</p>
+                    <h5>Purpura</h5>
+                    <p>Melhor equipe de todas!!!</p>
                 </div>
             </div>
             <div class="carousel-item">
                 <img class="d-block w-100" src="./publico/imgs/img3.jpeg" alt="Second slide">
                 <div class="carousel-caption d-none d-md-block m-auto col-4 rounded bg-caption">
-                    <h5>Rolezin</h5>
-                    <p>Nesse dia nós comemos coxinha</p>
+                    <h5>Club das Najas</h5>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="./publico/imgs/jaoPosando.jpeg" alt="Second slide">
+                <div class="carousel-caption d-none d-md-block m-auto col-4 rounded bg-caption">
+                    <h5>Abigos</h5>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="./publico/imgs/migas.jpeg" alt="Second slide">
+                <div class="carousel-caption d-none d-md-block m-auto col-4 rounded bg-caption">
+                    <h5>Monocromática</h5>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="./publico/imgs/natal.jpeg" alt="Second slide">
+                <div class="carousel-caption d-none d-md-block m-auto col-4 rounded bg-caption">
+                    <h5>Rolezin Aleatório</h5>
+                    <p>Foto focada somente na beleza do Sr.Cavalherie</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="./publico/imgs/piscina.jpeg" alt="Second slide">
+                <div class="carousel-caption d-none d-md-block m-auto col-4 rounded bg-caption">
+                    <h5>Fotinha Aleatória</h5>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="./publico/imgs/seminario.jpeg" alt="Second slide">
+                <div class="carousel-caption d-none d-md-block m-auto col-4 rounded bg-caption">
+                    <h5>Seminário da Dianice</h5>
+                    <p>Naquele ânimo de segunda</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="./publico/imgs/treno.jpeg" alt="Second slide">
+                <div class="carousel-caption d-none d-md-block m-auto col-4 rounded bg-caption">
+                    <h5>Rafael que se cuide KKK</h5>
+                    <p>Melhores amigos ou Crushes?</p>
                 </div>
             </div>
         </div>
